75f01733539bbb584fab351284ebd0addfa650f3
Pavel Kupert <disturbedelk@gmail.com>
2024-12-04 23:28:12 +0700

    fix container build dir

--- build-and-run-tests.sh
+++ build-and-run-tests.sh
@@ -1,2 +1,2 @@
-docker build -t test-server -f ./testing/Dockerfile .
+docker build -t test-server -f ./testing/Dockerfile ./testing
 docker run --env-file=token.env -p 8080:8080 --name test-server-container test-server
